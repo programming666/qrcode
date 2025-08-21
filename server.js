b5fb41c128a104ced3da22bc7b44986724b82e49
qinyining101 <code666@qinyining.cn>
2025-08-21 11:03:17 +0800

    Update server.js

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ app.post('/api/generate', async (req, res) => {
         }
 
         // 解密请求数据
-        let decryptedData,hello;
+        let decryptedData,hello,qwq;
         try {
             decryptedData = decryptData(encrypted);
         } catch (error) {
